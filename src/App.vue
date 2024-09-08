d9666bb4f780caee4da8c622a1a224188ef58ab5
JimYJ <yblues@hotmail.com>
2024-09-08 23:46:25 +0800

    frist push

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <CommonHeader/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
 </template>
 
 <script>
+// import CommonHeader from './components/CommonHeader.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    // CommonHeader,
     HelloWorld
   }
 }
@@ -21,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,163 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-container class="layout-container-demo">
+    <el-aside width="200px" class="main-menu">
+      <el-scrollbar>
+        <img alt="logo" width="200px" height="46px" src="../assets/logo.png">
+        <el-menu :default-openeds="['1', '3']">
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><message /></el-icon>Navigator One
+            </template>
+            <el-menu-item-group>
+              <template #title>Group 1</template>
+              <el-menu-item index="1-1">Option 1</el-menu-item>
+              <el-menu-item index="1-2">Option 2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group 2">
+              <el-menu-item index="1-3">Option 3</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>Option4</template>
+              <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon><icon-menu /></el-icon>Navigator Two
+            </template>
+            <el-menu-item-group>
+              <template #title>Group 1</template>
+              <el-menu-item index="2-1">Option 1</el-menu-item>
+              <el-menu-item index="2-2">Option 2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group 2">
+              <el-menu-item index="2-3">Option 3</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="2-4">
+              <template #title>Option 4</template>
+              <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon><setting /></el-icon>Navigator Three
+            </template>
+            <el-menu-item-group>
+              <template #title>Group 1</template>
+              <el-menu-item index="3-1">Option 1</el-menu-item>
+              <el-menu-item index="3-2">Option 2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group 2">
+              <el-menu-item index="3-3">Option 3</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="3-4">
+              <template #title>Option 4</template>
+              <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+    <el-container class="page">
+      <div aria-label="A complete example of page header">
+        <el-page-header @back="onBack">
+          <template #breadcrumb>
+            <el-breadcrumb separator="/">
+              <el-breadcrumb-item :to="{ path: './page-header.html' }">
+                homepage
+              </el-breadcrumb-item>
+              <el-breadcrumb-item>
+                <a href="./page-header.html">route 1</a>
+              </el-breadcrumb-item>
+              <el-breadcrumb-item>route 2</el-breadcrumb-item>
+            </el-breadcrumb>
+          </template>
+          <template #content>
+            <div class="flex items-center">
+              <el-avatar
+                class="mr-3"
+                :size="32"
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              />
+              <span class="text-large font-600 mr-3"> Title </span>
+              <span
+                class="text-sm mr-2"
+                style="color: var(--el-text-color-regular)"
+              >
+                Sub title
+              </span>
+              <el-tag>Default</el-tag>
+            </div>
+          </template>
+          <template #extra>
+            <div class="flex items-center">
+              <el-button>Print</el-button>
+              <el-button type="primary" class="ml-2">Edit</el-button>
+            </div>
+          </template>
+
+          <el-descriptions :column="3" size="small" class="mt-4">
+            <el-descriptions-item label="Username">
+              kooriookami
+            </el-descriptions-item>
+            <el-descriptions-item label="Telephone">
+              18100000000
+            </el-descriptions-item>
+            <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
+            <el-descriptions-item label="Remarks">
+              <el-tag size="small">School</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="Address">
+              No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+            </el-descriptions-item>
+          </el-descriptions>
+          <p class="mt-4 text-sm">
+            Element Plus team uses <b>weekly</b> release strategy under normal
+            circumstance, but critical bug fixes would require hotfix so the actual
+            release number <b>could be</b> more than 1 per week.
+          </p>
+        </el-page-header>
+      </div>
+    </el-container>
+  </el-container>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+<script setup>
+import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
+import { ElNotification as notify } from 'element-plus'
+const onBack = () => {
+  notify('Back')
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.layout-container-demo .el-header {
+  /* position: relative; */
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
 }
-ul {
-  list-style-type: none;
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+  background: var(--el-color-primary-light-8);
+}
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+.layout-container-demo .el-main {
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
 }
-a {
-  color: #42b983;
+.layout-container-demo .main-menu  {
+  border-right: 1px solid #dcdfe6; /* 设置右边的边框 */
+}
+.layout-container-demo .page{
+  margin-left: 20px;
 }
 </style>
