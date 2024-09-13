0a1e2add195e0d4c90b2bdfbed35aefa54021be6
Yangji <yblues@hotmail.com>
2024-09-13 19:12:03 +0800

    优化

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -9,12 +9,69 @@
         </el-menu>
     </el-aside>
     <InfoDetail v-if="$route.path==='/info/detail'"/>
+    <el-container class="task-list">
+      <el-page-header @back="onBack">
+      <template #breadcrumb>
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">
+            情报中心
+          </el-breadcrumb-item>
+          <el-breadcrumb-item>
+            <a href="./page-header.html">信息中心</a>
+          </el-breadcrumb-item>
+          <el-breadcrumb-item>信息详情</el-breadcrumb-item>
+        </el-breadcrumb>
+      </template>
+      <template #content>
+        <div class="flex items-center">
+          <span class="text-large font-600 mr-3">任务列表</span>
+        </div>
+      </template>
+      <el-table :data="tableData" >
+          <el-table-column prop="date" label="Date" min-width="200" />
+          <el-table-column prop="name" label="Name" min-width="200" />
+          <el-table-column prop="address" label="Address" min-width="300"/>
+      </el-table>
+      <div class="example-pagination-block">
+        <el-pagination
+          :page-size="20"
+          :pager-count="11"
+          layout="prev, pager, next"
+          :total="1000"
+        />
+      </div>
+    </el-page-header>
+    </el-container>
   </el-container>
 </template>
 
 <script setup>
 import InfoDetail from './InfoDetail.vue'
 import { List, Document, Setting } from '@element-plus/icons-vue'
+// const onBack = () => {
+// }
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 </script>
 
 <style scoped>
@@ -43,12 +100,22 @@ import { List, Document, Setting } from '@element-plus/icons-vue'
 .layout-container .main-menu  {
   border-right: 1px solid #dcdfe6; /* 设置右边的边框 */
 }
-.layout-container .info-detail{
+.layout-container .task-list ,.info-detail{
   margin-left: 20px;
   margin-top: 17px;
 }
 .main-menu{
   height: 97vh;
   background-color: #fff !important;
+}
+.example-pagination-block + .example-pagination-block {
+  margin-top: 50px;
+}
+.example-pagination-block{
+  margin-top: 15px;
+  margin-left: 80px;
+}
+.example-pagination-block .example-demonstration {
+  margin-bottom: 32px;
 }
 </style>
