6b0f1ae6548cc2ccb80fb7e4c08d476497abfa0e
JimYJ <yblues@hotmail.com>
2024-09-22 21:54:50 +0800

    wip 对话界面

--- src/components/WiseQuestions.vue
+++ src/components/WiseQuestions.vue
@@ -1,26 +1,108 @@
 <template class="body">
     <div class="body">
         <el-row :gutter="20" justify="center">
-            <el-col :span="24">
-                AI对你的回答
-            </el-col>
             <el-col :span="24" style="min-height: 300px;">
                 <div class="box">
                     <el-input resize="none" v-model="textarea" :rows="4" type="textarea" placeholder="输入你想要的内容" />
+                    <el-row class="row-bg" justify="space-between">
+                        <el-col :span="6" style="text-align: left;">
+                            <el-text class="mx-1" size="small">对档案提问:</el-text><el-switch v-model="options.useArchive" size="small" />
+                        </el-col>
+                        <el-col :span="12">
+                        </el-col>
+                        <el-col :span="6" style="text-align: right;">
+                            <el-icon :size="iconSize" class="icon">
+                                <Paperclip />
+                            </el-icon>
+                        </el-col>
+                    </el-row>
                 </div>
             </el-col>
         </el-row>
     </div>
-    <!-- <el-affix :offset="120" style="text-align: center;" class="affix"> -->
     <el-card style="max-width: 80px" class="affix">
-        <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+        <img alt="logo" width="50px" height="50px" src="../assets/logo1.png">
+        <el-divider />
+        <el-space :size="12" spacer=" " wrap direction="vertical">
+            <el-tooltip class="box-item" effect="dark" content="开启新会话" placement="left-end">
+                <el-icon :size="iconSize" class="icon">
+                    <ChatRound />
+                </el-icon>
+            </el-tooltip>
+            <el-tooltip class="box-item" effect="dark" content="开启新智问会话" placement="left-end">
+                <el-icon :size="iconSize" class="icon">
+                    <ChatDotRound />
+                </el-icon>
+            </el-tooltip>
+            <el-tooltip class="box-item" effect="dark" content="查看历史会话" placement="left-end">
+                <el-icon :size="iconSize" class="icon">
+                    <Clock />
+                </el-icon>
+            </el-tooltip>
+        </el-space>
+        <el-divider />
+        <el-space :size="12" spacer=" " wrap direction="vertical">
+            <el-tooltip class="box-item" effect="dark" content="创建自动更新的信息库" placement="left-end">
+                <el-icon :size="iconSize" class="icon">
+                    <Collection />
+                </el-icon>
+            </el-tooltip>
+        </el-space>
     </el-card>
-    <!-- </el-affix> -->
+    <div class="wxchatBorderRightMid">
+        <div style="margin-left: 30px;margin-top: 15px;">
+            <el-row>
+                <el-col :span="1" :pull="1">
+                    <el-avatar shape="square" :src="bot" style="width: 35px;height: 35px;"></el-avatar>
+                </el-col>
+                <el-col :span="20" :pull="1">
+                    <div class="chatPop1">
+                        <span style="line-height: 23px;">你好文本属性来选择属性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性</span>
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
+        <div style="margin-left: 30px;margin-top: 15px;">
+            <el-row>
+                <el-col :span="20">
+                    <div class="chatPop2" :push="3">
+                        <span style="line-height: 23px;">你好文本属性来选择属性来选择属性来选择属性来选择属性来选择属性来性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性</span>
+                    </div>
+                </el-col>
+                <el-col :span="1">
+                    <el-avatar shape="square" :src="squareUrl" style="width: 35px;height: 35px;"></el-avatar>
+                </el-col>
+            </el-row>
+        </div>
+        <div style="margin-left: 30px;margin-top: 15px;">
+            <el-row>
+                <el-col :span="1" :pull="1">
+                    <el-avatar shape="square" :src="bot" style="width: 35px;height: 35px;"></el-avatar>
+                </el-col>
+                <el-col :span="20" :pull="1">
+                    <div class="chatPop1">
+                        <span style="line-height: 23px;">你好文本属性来选择属性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性性来选择属性来选择属性</span>
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
 <script  setup>
+import { Clock, ChatDotRound, ChatRound, Collection, Paperclip } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 const textarea = ref('')
+
+const iconSize = 25
+
+const options = ref({
+    useInternet: true,
+    useArchive: false,
+})
+
+const bot = "../assets/logo1.png"
+
 </script>
 
 <style scoped>
@@ -41,6 +123,7 @@ div /deep/ .el-textarea__inner {
     border-radius: 15px;
     border: 0px;
     box-shadow: none;
+    color: #a1a1a1;
 }
 
 div /deep/ .el-textarea__inner:hover {
@@ -66,7 +149,85 @@ div /deep/ .el-textarea__inner::-webkit-scrollbar-track {
 .affix {
     position: fixed;
     top: 30%;
-    right: 20px; /* 左侧定位 */
+    right: 20px;
+}
+
+div /deep/ .el-card__body {
+    padding: 10px;
+}
+
+.icon {
+    color: #a1a1a1;
+}
+.icon:hover {
+    color: var(--el-color-primary);
+    transform: scale(1.1);
+}
+.icon:active {
+    color: var(--el-color-primary);
+    transform: scale(1.2);
+}
+
+.chatPop1 :hover {
+    /* background-color: #c6c8eb; */
+}
+
+.chatPop1 span {
+    background-color: #e8e9f8;
+    padding: 5px 8px;
+    display: inline-block;
+    border-radius: 10px;
+    margin: 0px 0 10px 10px;
+    position: relative;
+    float: left;
+    font-size: small;
+    text-align: left;
+    max-width: 800px;
+}
+
+.chatPop1 span::after {
+    content: "";
+    position: absolute;
+    top: 8px;
+    left: -16px;
+}
+
+.chatPop2 :hover {
+    background-color: var(--el-color-primary);
+}
+
+.chatPop2 span {
+    background-color: var(--el-color-primary);
+    padding: 5px 8px;
+    display: inline-block;
+    border-radius: 10px;
+    margin: 0px 5px 10px 10px;
+    position: relative;
+    text-align: left;
+    float: right;
+    color: #fff;
+    font-size: small;
+    max-width: 800px;
+}
+
+.chatPop2 span::after {
+    content: "";
+    position: absolute;
+    top: 8px;
+    right: -16px;
+    transform: rotateY(180deg);
+}
+
+.wxchatBorderRightMid {
+    width: 1024px;
+    height: 428px;
+    /* background-color: #f5f5f5; */
+    display: block;
+    margin-right: -20px;
+    position: fixed;
+    top: 5px;
+    left: 50%; /* 将元素的左边缘设置为视口宽度的50% */
+    transform: translate(-50%);
 }
 </style>
 
