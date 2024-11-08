ca7283bb83a8935d584ecad1d7d243d83ba22330
JimYJ <yblues@hotmail.com>
2024-11-08 16:29:52 +0800

    任务信息联调 done

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -29,31 +29,31 @@
             <el-card shadow="never" body-style="background-color: #fcfcfc;">
                 <el-row>
                     <el-col :span="6">
-                        <el-statistic title="总任务数" :value="268500" value-style="font-size:30px">
-                            <template #title>
-                                <span style="font-size: 14px;font-weight: 100;">总任务数</span>
-                            </template>
-                        </el-statistic>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-statistic :value="138" title="正在执行的任务" value-style="font-size:30px">
+                        <el-statistic :value="taskInfo.active_task_count" title="正在执行的任务" value-style="font-size:30px">
                             <template #title>
                                 <span style="font-size: 14px;font-weight: 100;">正在执行的任务/总任务</span>
                             </template>
-                            <template #suffix>/100</template>
+                            <template #suffix>/{{taskInfo.task_count}}</template>
                         </el-statistic>
                     </el-col>
                     <el-col :span="6">
-                        <el-statistic title="已经抓取的情报" :value="outputValue" value-style="font-size:30px">
+                        <el-statistic title="已经抓取的情报" :value="taskInfo.archive_docs_count" value-style="font-size:30px">
                             <template #title>
                                 <span style="font-size: 14px;font-weight: 100;">已经抓取的情报</span>
                             </template>
                         </el-statistic>
                     </el-col>
                     <el-col :span="6">
-                        <el-statistic title="已建档案" :value="562" value-style="font-size:30px">
+                        <el-statistic title="已建档案" :value="taskInfo.archive_count" value-style="font-size:30px">
                             <template #title>
                                 <span style="font-size: 14px;font-weight: 100;">已建档案</span>
+                            </template>
+                        </el-statistic>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-statistic title="文档资源数" :value="taskInfo.archive_docs_res_count" value-style="font-size:30px">
+                            <template #title>
+                                <span style="font-size: 14px;font-weight: 100;">文档资源数</span>
                             </template>
                         </el-statistic>
                     </el-col>
@@ -67,10 +67,15 @@
                         <el-tag size="small" :type="row.task_status ? 'success' : 'warning'">{{ showStatus(row.task_status) }}</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column prop="task_ststus" label="执行时间" min-width="300" />
+                <el-table-column prop="task_ststus" label="执行时间" min-width="200">
+                    <template #default="{ row }">
+                        <el-tag size="small" type="primary">{{ showCycleType(row.exec_type,row.cycle_type,row.week_days,row.exec_time) }}</el-tag>
+                    </template>
+                </el-table-column>
                 <el-table-column prop="task_ststus" label="抓取模式" min-width="80">
                     <template #default="{ row }">
                         <el-tag size="small" type="primary">{{ showCrawlMode(row.crawl_mode) }}</el-tag>
+
                     </template>
                 </el-table-column>
                 <el-table-column prop="extraction_model" label="抽取模型" min-width="250">
@@ -98,8 +103,8 @@ import CreateTask from './CreateTask.vue'
 import { Search } from '@element-plus/icons-vue'
 import { ElMessage } from 'element-plus'
 import { ref } from 'vue'
-import { useTransition } from "@vueuse/core";
-import { post } from '../http';
+// import { useTransition } from "@vueuse/core";
+import { post, get } from '../http';
 const onBack = () => {
     window.history.back();
 }
@@ -107,11 +112,13 @@ const showCreateTask = ref(false)
 const hideCreateTask = (vision) => {
     showCreateTask.value = vision;
 };
-const source = ref(0);
-const outputValue = useTransition(source, {
-    duration: 1500,
+const taskInfo = ref({
+    archive_count: 0,
+    archive_docs_count: 0,
+    archive_docs_res_count: 0,
+    task_count: 0,
+    active_task_count: 0,
 });
-source.value = 172000;
 let pageInfo = ref({
     keyword: "",
     current: 1,
@@ -126,20 +133,28 @@ let pages = {
 }
 // 翻页 
 const changePage = (newPage) => {
-    pageInfo.value.current = newPage
-    pages.current = pageInfo.value.current
-    getData()
+    pageInfo.value.current = newPage;
+    pages.current = pageInfo.value.current;
+    getData();
 };
 // 获取数据
 const getData = () => {
     post("/task/list", { keyword: pageInfo.value.keyword }, pages).then(res => {
-        console.log(res);
+        // console.log(res);
         if (res.success) {
             pageInfo.value = res.data
         } else {
             ElMessage.error('加载任务列表失败')
         }
-    }).catch()
+    }).catch();
+    get("/task/info").then(res => {
+        // console.log(res);
+        if (res.success) {
+            taskInfo.value = res.data
+        } else {
+            ElMessage.error('加载任务信息失败')
+        }
+    }).catch();
 }
 getData();
 
@@ -152,13 +167,42 @@ const getApiType = (apiType) => {
     return apiTypeMapping[apiType] || '未知类型';
 };
 const showStatus = (value) => {
-    console.log(value)
     return value === false ? '关闭' : value === true ? '启用' : '数据错误';
 }
 const showCrawlMode = (value) => {
-    console.log(value)
-    return value === false ? '地址抓取' : value === true ? '启用' : '数据错误';
+    return value === "1" ? '地址抓取' : value === "2" ? '智能抓取' : '数据错误';
 }
+const showCycleType = (execType, cycleType, weekDays, execTime) => {
+    let srt = "";
+    if (execType === "1") {
+        return '单次执行';
+    } else {
+        if (cycleType === "1") {
+            srt = "每日";
+        } else if (cycleType === "2") {
+            srt = "每周";
+            srt += getWeekday(weekDays);
+        }
+        srt += " " + execTime + " 执行";
+    }
+    return srt
+}
+// 转日期
+const getWeekday = (weekDays) => {
+    let srt = "";
+    if (weekDays == null) {
+        return srt
+    }
+    for (let i = 0; i < weekDays.length; i++) {
+        if (i === 0) {
+            srt += chineseNumbers[weekDays[i] - 1]
+        } else {
+            srt += "、" + chineseNumbers[weekDays[i] - 1]
+        }
+    }
+    return srt + "的"
+}
+const chineseNumbers = ['一', '二', '三', '四', '五', '六', '日'];
 </script>
 
 <style scoped>
--- src/http.js
+++ src/http.js
@@ -29,7 +29,7 @@ export const post = (url, data, params) => {
 http.interceptors.response.use(
     response => {
         // 对响应数据做些什么
-        console.log("响应报文:", response)
+        // console.log("响应报文:", response)
         return response.data; // 返回实际数据
     },
     error => {
